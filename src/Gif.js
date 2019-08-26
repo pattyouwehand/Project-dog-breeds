20702c0e7c70526d4c8812954918c8cd86a3d5f9
LPascholatti <lucaspascholatti@gmail.com>
2019-08-26 10:39:41 +0200

    cleaned up the code and added a gif

---
+++ src/Gif.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Gif extends Component {
+
+    render() {
+        return (
+            <img src="https://media.giphy.com/media/mCRJDo24UvJMA/giphy.gif" alt="gif">
+            </img>
+        )
+    }
+}
