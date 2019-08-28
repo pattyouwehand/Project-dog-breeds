9171fabd2d578c32e0bf5fc90ffb21b3e0300bfc
pattyouwehand <pattyouwehand@gmail.com>
2019-08-28 12:32:13 +0200

    api fetch

--- src/components/GameOneContainer.js
+++ src/components/GameOneContainer.js
@@ -7,10 +7,17 @@ export default class GameOneContainer extends React.Component {
   state = {}
 
   componentDidMount(){
-    request('https://dog.ceo/api/breeds/image/random')
-    .then(response => this.setState({dog: response.body}))
+    request
+    .get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      console.log("RESPONSE", response)
+      this.setState({random: response.body})
+    })
+    .catch(console.error)
   }
 
-
+  render(){
+    if(!this.state.random) return "Loading.."
+    return <GameOnList random={this.state.random} />
+  }
 }
-
