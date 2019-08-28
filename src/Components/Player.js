1ee1cce7968f6a82267631c6cf334f2c59b9b9ca
LPascholatti <lucaspascholatti@gmail.com>
2019-08-28 16:13:24 +0200

    fixed the add players and score into redux

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -8,14 +8,16 @@ export default class Player extends Component {
 
   render() {
     console.log("Player Props", this.props)
-    const { name, score, id, incrementScore } = this.props
+    const { name, score, id } = this.props
     return (
-      <ul className="player">
+      <ul className="players">
         <p className="name"> {name}</p>
         <p className="name"> Score: {score}</p>
         <p className="name"> ID: {id} </p>
-        <button onClick={() => incrementScore(name)}>Add Points +</button>
+        {/* <button onClick={() => incrementScore(name)}>Add Points +</button> */}
       </ul>
     )
   }
 }
+
+// const { name, score, id, incrementScore } = this.props
