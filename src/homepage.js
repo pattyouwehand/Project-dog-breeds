a4af93cbb43560057084743a7fc2d6fb1cbd76ef
LPascholatti <lucaspascholatti@gmail.com>
2019-08-29 14:35:22 +0200

    added styling in the home

--- src/homepage.js
+++ src/homepage.js
@@ -9,6 +9,12 @@ export default class Homepage extends Component {
         <br />
         <h4> Enjoy the app. You can start by checking our breed list. Click in 'Dog List' for more information. </h4>
         <p> Welcome to Dog Breeds App. Here you can study the name of different dog breeds and practice your knowledge with our games.</p>
+        <br />
+        <br />
+        <img src="https://media.giphy.com/media/1kkxWqT5nvLXupUTwK/giphy.gif" alt="gif2">
+        </img>
+        <br />
+        <br />
       </div>
     )
   }
