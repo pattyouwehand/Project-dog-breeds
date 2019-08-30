bc396954d66200082be04ab777d8320fa43f1b35
LPascholatti <lucaspascholatti@gmail.com>
2019-08-30 15:10:36 +0200

    commiting master build

--- src/reducers/breedgametwo.js
+++ src/reducers/breedgametwo.js
@@ -1,13 +1,6 @@
 const reducer = (state = [], action) => {
-<<<<<<< HEAD
-  // console.log("REDUCER ACTION STORE_PLAYER", action)
   switch (action.type) {
   case 'STORE_BREED': {
-    // console.log("Test Reducer Store Player")
-=======
-  switch (action.type) {
-  case 'STORE_BREED': {
->>>>>>> master
     return [...state, action.payload]
   }
   default:
--- src/reducers/gametwoimg.js
+++ src/reducers/gametwoimg.js
@@ -1,14 +1,7 @@
 const reducer = (state = [], action) => {
-<<<<<<< HEAD
-  // console.log("func test", action)
-  switch (action.type) {
-  case 'STORE_GAME_IMAGES': {
-    // console.log("Test Reducer")
-=======
   
   switch (action.type) {
   case 'STORE_GAME_IMAGES': {
->>>>>>> master
     return action.payload
   }
   default:
